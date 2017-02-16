8786bb7657a9f416fa4c0e0120335d9b13008d5d
Ian Storm <99039737@mydavinci.nl>
2017-02-16 14:30:43 +0100

    update

--- mastermind js.js
+++ mastermind js.js
@@ -1,10 +1,25 @@
 var color = ["red", "yellow", "blue", "limegreen", "brown", "cyan"];
+var code = [];
+var ball = 1;
+var row = 1;
+var playedcode = [];
 
-function player() {
-	
+function gencode() {
+	var number;
+	var i;
+	for (i = 0; i < 4; i++){
+		number = Math.floor(Math.random() * 6);
+		console.log(number);
+		code.push (number);
+	}
+	for (i = 0; i < 4; i++){
+		code.splice(i , 1 , color[code[i]])
+	}
+	console.log(code);
 }
 
-function random() {
-	Math.floor((Math.random() * 6) + 1);
+function addcolor(color) {
+	playedcode.push (color);
+	var x = document.getElementsByClassName("dot1");
+	console.log(x);
 }
-
